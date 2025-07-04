be5cf1b54c498f68e9a3a4a9590a8475bae8d9c0
troopdegen <mel@innvertir.com>
2025-07-04 01:21:11 -0600

    minor hero + navbar updates

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -1,14 +1,14 @@
 import { Button } from '@/components/ui/button'
-import { SparkleIcon } from 'lucide-react'
+import { SparklesIcon } from 'lucide-react'
 
 export default function HeroSection() {
   return (
-    <div className="relative w-full py-20 md:py-32">
+    <div className="relative w-full py-20 md:py-28">
       <div className="container mx-auto space-y-16 px-4 text-center">
         {/* Título Principal */}
         <div className="mx-auto max-w-4xl">
           <h1 className="text-4xl leading-tight font-semibold text-foreground md:text-6xl lg:text-7xl">
-            La comunidad de{' '}
+            El club de{' '}
             <span className="inline-block -rotate-1 transform rounded-lg bg-accent px-4 py-2 text-white">
               Builders
             </span>
@@ -24,9 +24,9 @@ export default function HeroSection() {
         <div>
           <Button
             size="xl"
-            className="transform rounded-full bg-primary px-16 py-8 text-2xl font-bold text-white shadow-lg transition-all duration-200 hover:scale-105 hover:bg-primary/90"
+            className="transform rounded-full bg-primary px-16 py-8 text-3xl font-bold text-white shadow-lg transition duration-300 ease-in-out hover:scale-105 hover:bg-primary/90"
           >
-            <SparkleIcon className="mr-2 -ml-2 h-6 w-6" /> Únete
+            Quiero unirme <SparklesIcon className="ml-2 h-6 w-6" />
           </Button>
         </div>
 
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -34,15 +34,15 @@ export default function Navbar() {
   const pathname = usePathname()
 
   return (
-    <header className="top-0 h-20 w-full bg-background">
+    <header className="top-0 h-24 w-full bg-background">
       <div className="mx-auto flex h-full w-full max-w-3xl items-center justify-between p-4 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-5 lg:px-8">
-        <Link className="flex w-40 items-center" href="/">
+        <Link className="flex w-36 items-center" href="/">
           <Image
             src="/images/logos/frutero-logo.svg"
             alt="Frutero logo"
             width={128}
             height={128}
-            className="w-full transition duration-300 ease-in-out hover:scale-105"
+            className="-mt-1 w-full transition duration-300 ease-in-out hover:scale-105"
           />
           <span className="sr-only">Frutero Club</span>
         </Link>
@@ -52,7 +52,7 @@ export default function Navbar() {
               (menuItem, index) => (
                 <Link
                   key={`${menuItem.displayText}-menuItem-${index}`}
-                  className={`inline-flex items-center justify-center px-4 py-2 font-funnel text-lg font-medium text-foreground transition-colors hover:text-primary focus:text-primary focus:outline-none ${
+                  className={`inline-flex items-center justify-center px-4 py-2 font-funnel text-xl font-medium text-foreground transition-colors hover:text-primary focus:text-primary focus:outline-none ${
                     pathname === menuItem.href &&
                     'pointer-events-none underline decoration-primary decoration-[1.5px] underline-offset-[6px] hover:!text-foreground'
                   }`}
@@ -66,7 +66,10 @@ export default function Navbar() {
           </nav>
         </div>
         <div className="hidden lg:flex lg:justify-end">
-          <AuthButton className="transition duration-300 ease-in-out hover:scale-105">
+          <AuthButton
+            size="lg"
+            className="transition duration-300 ease-in-out hover:scale-105"
+          >
             <SparkleIcon className="mr-2 -ml-2 h-4 w-4" /> Únete
           </AuthButton>
         </div>
